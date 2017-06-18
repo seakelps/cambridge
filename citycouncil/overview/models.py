85f38a979027c388e9a5e9df016c086c58653f8c
kels <chondrus@users.noreply.github.com>
2017-06-18 18:22:53 -0400

    initial candidate table/model and using that in the base header

--- citycouncil/overview/models.py
+++ citycouncil/overview/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    fullname = models.CharField(max_length=200)
+    shortname = models.CharField(max_length=200)
+
+    # restricting this field
+    pronoun_choices = (
+        ('she',  'She'),
+        ('he',   'He'),
+        ('they', 'They'),
+    )
+    pronoun = models.CharField(max_length=4, choices=pronoun_choices)
+
+    # the status of the candidate can be determined from these
+    # two boolean fields
+    is_running = models.NullBooleanField()
+    is_incumbent = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.fullname
--- citycouncil/overview/views.py
+++ citycouncil/overview/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Candidate
+
 
 # asdasd
 def index(request):
-    return render(request, 'overview/index.html')
+    incumbents = Candidate.objects.filter(is_incumbent=True).order_by('fullname')
+    non_incumbents = Candidate.objects.filter(is_incumbent=False).order_by('fullname')
+    context = {'incumbents': incumbents, 'non_incumbents': non_incumbents}
+    return render(request, 'overview/index.html', context)
+
+
+
+# todo: detail?
