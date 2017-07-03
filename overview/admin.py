6b25b742135ff947d7c70520fc627a3f3ad07424
Alex Riina <alex.riina@gmail.com>
2017-07-03 00:19:02 -0400

    add website admin filter

--- overview/admin.py
+++ overview/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 from .models import Candidate
 
 
+class HasWebsite(admin.SimpleListFilter):
+    title = "Has Website"
+    parameter_name = "has_website"
+
+    def lookups(self, request, model_admin):
+        return (("yes", "Yes"), ("no", "No"))
+
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            return queryset.exclude(website="")
+        elif self.value() == "no":
+            return queryset.filter(website="")
+        else:
+            return queryset
+
+
 class CandidateAdmin(admin.ModelAdmin):
     readonly_fields = ('headshot', )
     list_display = ('fullname', 'is_running', 'is_incumbent')
-    list_filter = ('is_running', 'is_incumbent')
+    list_filter = ('is_running', 'is_incumbent', HasWebsite)
     prepopulated_fields = {"slug": ("fullname",)}
 
     def headshot(self, instance):
