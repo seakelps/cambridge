99c754b80df36f311e664707c0f59fc84c3de444
Alex Riina <alex.riina@gmail.com>
2017-07-04 14:47:39 -0400

    add warnings about not running / maybe running

--- overview/context_processors.py
+++ overview/context_processors.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from .models import Candidate
 
 
@@ -5,3 +6,9 @@ def header(request):
     incumbents = Candidate.objects.filter(is_incumbent=True).order_by('fullname')
     non_incumbents = Candidate.objects.filter(is_incumbent=False).order_by('fullname')
     return {'incumbents': incumbents, 'non_incumbents': non_incumbents}
+
+
+def constants(request):
+    return {
+        "CONTACT_EMAIL": settings.CONTACT_EMAIL,
+    }
