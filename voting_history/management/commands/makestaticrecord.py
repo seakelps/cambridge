7d92feaa39b183ebf5d4b65f72f3691ff4c8622d
Alex Riina <alex.riina@gmail.com>
2017-07-13 21:19:19 -0400

    add key_votes to datatables with checkbox filter

--- voting_history/management/commands/makestaticrecord.py
+++ voting_history/management/commands/makestaticrecord.py
@@ -2,6 +2,9 @@ import os.path
 import csv
 import re
 import json
+from functools import reduce
+from operator import mul
+from collections import Counter
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -14,12 +17,13 @@ class Command(BaseCommand):
     # we're checking it in so it doesn't matter much
     csv_source = os.path.join(settings.BASE_DIR, 'static', 'voting_record.csv')
     json_out = os.path.join(settings.BASE_DIR, 'static', 'voting_record.json')
+    junk = re.compile(r'\b({})\b'.format('|'.join([
+        "A communication was received", "relative to",
+        "the appropriation of", "the", "a", "of"])))
 
     def add_short_description(self, row):
-        self.junk = self.junk or re.compile(r'\b({})\b'.format('|'.join([
-            "A communication was received", "relative to",
-            "the appropriation of", "the", "a", "of"])))
-
+        """ reduce some junk where we can from the short descriptions so the
+        preview is more useful """
         short_description = row['item_description'].split("City Manager", 1)[-1]
         short_description = self.junk.sub('', short_description)
         short_description = re.sub("\s+", " ", short_description).strip(', ')
@@ -29,12 +33,26 @@ class Command(BaseCommand):
         else:
             row['short_description'] = ''  # js crashes otherwise
 
+    def add_key_vote(self, row):
+        """ calculate key votes based on "controversialness". hopefully
+        designed to work for larger voter bases """
+        vals = Counter(row[c] for c in self.candidates)
+        vals.pop("Absent", "")
+
+        # normal product runs into issues floor effects, so boosting by 10 to get
+        # it favor many splits of equal size. Might be worth considering the
+        # Absents if this isn't a good enough seed
+        row['key_vote'] = reduce(mul, (10 + x for x in vals.values())) > len(self.candidates) + 10
+
     def handle(self, *args, **kwargs):
         with open("static/voting_record.csv", "r") as fp:
             reader = csv.DictReader(fp)
             rows = list(reader)
 
+        self.candidates = reader.fieldnames[5:]
+
         for row in rows:
             self.add_short_description(row)
+            self.add_key_vote(row)
 
         json.dump({"data": rows}, open("static/voting_record.json", "w"), indent=True)
