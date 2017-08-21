945400230fd7e7b51ad97bc4f9fd21504a94d929
kels <chondrus@users.noreply.github.com>
2017-08-20 22:19:58 -0400

    sort in header context by running type; add basic styling to dropdown for that

--- overview/context_processors.py
+++ overview/context_processors.py
@@ -3,8 +3,8 @@ from .models import Candidate
 
 
 def header(request):
-    incumbents = Candidate.objects.filter(is_incumbent=True).order_by('fullname')
-    non_incumbents = Candidate.objects.filter(is_incumbent=False).order_by('fullname')
+    incumbents = Candidate.objects.filter(is_incumbent=True).order_by('-is_running', 'fullname')
+    non_incumbents = Candidate.objects.filter(is_incumbent=False).order_by('-is_running', 'fullname')
     return {'incumbents': incumbents, 'non_incumbents': non_incumbents}
 
 
