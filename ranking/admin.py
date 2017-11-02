33bd4ea53e54b73a0f8c51caaa967d1035346192
Alex Riina <alex.riina@gmail.com>
2017-11-02 19:58:34 -0400

    add filters for rankedlist lenght

--- ranking/admin.py
+++ ranking/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import RankedList, RankedElement
 
@@ -7,9 +8,37 @@ class CandidateInline(admin.StackedInline):
     model = RankedElement
 
 
+class LengthFilter(admin.SimpleListFilter):
+    """ filter ranked lists by length"""
+    title = 'Length'
+    parameter_name = 'length'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('empty', 'Empty'),
+            ('non-empty', 'Non-Empty'),
+            ('full', 'Full'),
+        )
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return queryset
+
+        with_count = queryset.annotate(count=Count("annotated_candidates"))
+
+        if self.value() == "empty":
+            return with_count.filter(count=0)
+        elif self.value() == "non-empty":
+            return with_count.filter(count__gt=0)
+        elif self.value() == "full":
+            return with_count.filter(count__gte=9)
+        else:
+            return with_count.all()
+
+
 class RankedListAdmin(admin.ModelAdmin):
     raw_id_fields = ['owner']
-    list_filer = ['public']
+    list_filter = ['public', LengthFilter]
     date_hierarchy = 'last_modified'
     list_display = ['name', 'owner', 'last_modified', 'public']
     inlines = [CandidateInline]
