0c05b28c249a2743227a7f36f84183eb4f41194b
Alex Riina <alex.riina@gmail.com>
2017-11-05 14:37:24 -0500

    fix sortby field

--- overview/views.py
+++ overview/views.py
@@ -58,10 +58,10 @@ class CandidateDetail(DetailView):
 
         context['others_articles'] = self.object.pressarticlecandidate_set\
             .filter(display=True, candidate_is_the_author=False)\
-            .select_related("pressarticle").order_by("-date")
+            .select_related("pressarticle").order_by("-pressarticle__date")
         context['own_articles'] = self.object.pressarticlecandidate_set\
             .filter(display=True, candidate_is_the_author=True)\
-            .select_related("pressarticle").order_by("-date")
+            .select_related("pressarticle").order_by("-pressarticle__date")
 
         if self.object.cpf_id:
             try:
