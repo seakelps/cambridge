fb3e64e8ab5fd4519690557e775fb3fc55bc54f3
seakelps <you@example.com>
2019-07-11 21:08:13 -0400

    use the hide to hide candidates, and also update the year

--- overview/context_processors.py
+++ overview/context_processors.py
@@ -3,8 +3,8 @@ from .models import Candidate
 
 
 def header(request):
-    incumbents = Candidate.objects.filter(is_incumbent=True).order_by('-is_running', 'fullname')
-    non_incumbents = Candidate.objects.filter(is_incumbent=False).order_by('-is_running', 'fullname')
+    incumbents = Candidate.objects.filter(is_incumbent=True, hide=False).order_by('-is_running', 'fullname')
+    non_incumbents = Candidate.objects.filter(is_incumbent=False, hide=False).order_by('-is_running', 'fullname')
     return {'incumbents': incumbents, 'non_incumbents': non_incumbents}
 
 
