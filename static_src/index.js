6bef823085e6584a6e29bfa35ff24fc901a6e7ee
Alex Riina <alex.riina@gmail.com>
2019-08-24 21:11:09 -0400

    bring over stylesheets from state.vote

--- static_src/index.js
+++ static_src/index.js
@@ -3,6 +3,7 @@ import './jquery.ui.touch-punch.min.js'
 import 'knockout-sortable'
 import 'slidereveal'
 import 'bootstrap'
+import style from './stylesheets/application.sass'
 import view_model from './candidate_sidebar'
 
 export { view_model }
--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,18 @@ module.exports = {
       {
         test: /\.css$/,
         loaders: ["style-loader","css-loader"]
-      }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ]
   }
 };
