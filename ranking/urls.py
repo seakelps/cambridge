7d4323d4c12fe138b18dcf0ae7a8a3143f57e4d0
Alex Riina <alex.riina@gmail.com>
2019-10-01 21:10:40 -0400

    mark urls as regexes

--- ranking/urls.py
+++ ranking/urls.py
@@ -6,12 +6,12 @@ from .views import (
 
 urlpatterns = [
     url("^mine/$", view=MyList.as_view(), name="my_ranking"),
-    url("^mine/append/(?P<slug>[-_\w]+)$", view=append_to_ballot, name="append_to_ballot"),
+    url(r"^mine/append/(?P<slug>[-_\w]+)$", view=append_to_ballot, name="append_to_ballot"),
     url("^mine/make-public/$", view=MakePublic.as_view(), name="make_public"),
     url("^mine/make-ordered/$", view=MakeOrdered.as_view(), name="make_ordered"),
 
-    url("^notes/(?P<slug>[-_\w]+)/$", view=UpdateNote.as_view(), name="update_note"),
-    url("^delete/(?P<slug>[-_\w]+)/$", view=delete_note, name="delete_note"),
+    url(r"^notes/(?P<slug>[-_\w]+)/$", view=UpdateNote.as_view(), name="update_note"),
+    url(r"^delete/(?P<slug>[-_\w]+)/$", view=delete_note, name="delete_note"),
     url("^explore/", include([
         url(r"^$", view=RankedListExplore.as_view(), name="list_explore"),
         url(r"^(?P<slug>[-_\w]+)/$", view=RankedListDetail.as_view(), name="list_explore"),
