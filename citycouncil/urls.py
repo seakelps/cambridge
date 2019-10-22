9ac31ea850112ecff08473a267c183037aec0f4a
Alex Riina <alex.riina@gmail.com>
2019-10-21 23:13:07 -0400

    remove stray debug toolbar lines

--- citycouncil/urls.py
+++ citycouncil/urls.py
@@ -10,7 +10,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-import debug_toolbar
 
 
 class HowToVote(TemplateView):
@@ -41,10 +40,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
-    ] + urlpatterns
