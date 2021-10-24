aa2acd95e0bd33ed0b5bb927e758586fcc0b516b
Alex Riina <alex.riina@gmail.com>
2021-10-24 15:33:41 -0400

    strip more junk from acela output

--- voting_history/management/commands/prepare_voting_record_acela.py
+++ voting_history/management/commands/prepare_voting_record_acela.py
@@ -3,50 +3,46 @@ for voting records scraped entirely from acela site
 """
 
 import argparse
+import re
 import csv
-import tqdm
 import json
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("csv_file", type=argparse.FileType("r"), metavar="<voting-record-$date.csv>")
+        parser.add_argument(
+            "csv_file", type=argparse.FileType("r"), metavar="<voting-record-$date.csv>"
+        )
 
     def handle(self, *args, csv_file, **kwargs):
         reader = csv.DictReader(csv_file)
-        # meeting_id
-# item_id
-# date
-# item_description
-# item_body
-# Dennis J. Carlone
-# Alanna Mallon
-# Marc C. McGovern
-# Sumbul Siddiqui
-# E. Denise Simmons
-# Timothy J. Toomey
-# Quinton Zondervan
-# Patty Nolan
-# Jivan Sobrinho-Wheeler
 
-        voting_history = [{
-            "resolutionid": row["item_id"],
-            "full_text": row["item_description"] + " " + row["item_body"],
-            "meetingdate": row["date"],
+        voting_history = []
+        for row in reader:
+            full_text = row["item_description"] + " " + row["item_body"]
+            full_text = re.sub(r"\n+\|?\n*", " ", full_text)
+            full_text = re.sub(r" {2,}", " ", full_text)
+            full_text = full_text.strip()
 
-            "resolutionshorttitle": row["item_description"],
+            voting_history.append(
+                {
+                    "meetingid": row["meeting_id"],
+                    "resolutionid": row["item_id"],
+                    "full_text": full_text,
+                    "meetingdate": row["date"],
+                    "resolutionshorttitle": row["item_description"].strip(),
+                    "dennis_carlone": row["Dennis J. Carlone"],
+                    "alanna_mallon": row["Alanna Mallon"],
+                    "marc_mcgovern": row["Marc C. McGovern"],
+                    "sumbul_siddiqui": row["Sumbul Siddiqui"],
+                    "denise_simmons": row["E. Denise Simmons"],
+                    "tim_toomey": row["Timothy J. Toomey"],
+                    "quinton_zondervan": row["Quinton Zondervan"],
+                    "jivan_sobrinho-wheeler": row["Jivan Sobrinho-Wheeler"],
+                    "patty_nolan": row["Patty Nolan"],
+                }
+            )
 
-            "dennis_carlone": row["Dennis J. Carlone"],
-            "alanna_mallon": row["Alanna Mallon"],
-            "marc_mcgovern": row["Marc C. McGovern"],
-            "sumbul_siddiqui": row["Sumbul Siddiqui"],
-            "denise_simmons": row["E. Denise Simmons"],
-            "tim_toomey": row["Timothy J. Toomey"],
-            "quinton_zondervan": row["Quinton Zondervan"], 
-            "jivan_sobrinho-wheeler": row["Jivan Sobrinho-Wheeler"], 
-            "patty_nolan": row["Patty Nolan"], 
-        } for row in reader]
-
-        with open('static/voting_record.prepared.json', 'w') as fp:
-            json.dump({'data': voting_history}, fp, indent=2)
+        with open("static/voting_record.prepared.json", "w") as fp:
+            json.dump({"data": voting_history}, fp, indent=2)
