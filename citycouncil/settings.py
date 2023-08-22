0b1573a01d60b9f16c344e125f9e713f7073640c
Alex Riina <alex.riina@gmail.com>
2023-08-22 01:22:48 -0400

    add dockerfile

--- citycouncil/settings.py
+++ citycouncil/settings.py
@@ -188,10 +188,3 @@ LOGOUT_REDIRECT_URL = '/'
 # by commenting out EMAIL_BACKEND and copying all the production email settings.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 GOOGLE_EMBED_API_KEY = os.getenv('GOOGLE_EMBED_API_KEY')
-
-django_heroku.settings(
-    locals(),
-    databases=not os.environ.get('CI'),
-    staticfiles=not os.environ.get('CI'),
-    test_runner=False,  # doesn't work with sqlite3
-)
---
+++ docker_settings.py
@@ -0,0 +1,26 @@
+import os
+from citycouncil.settings import *
+
+ALLOWED_HOSTS = ["*"]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+DEBUG = True
+
+
+class InvalidString(str):
+    def __mod__(self, other):
+        from django.template.base import TemplateSyntaxError
+        raise TemplateSyntaxError(
+            "Undefined variable or unknown value for: \"%s\"" % other)
+
+
+TEMPLATES[0]["OPTIONS"]['string_if_invalid'] = InvalidString("%s")
+# INSTALLED_APPS.append("django_extensions")
