91d6e383270dea8b2094404bdba72cf0e2bc2e80
Alex Riina <alex.riina@gmail.com>
2023-08-22 01:17:27 -0400

    fix crispy after upgrade

--- citycouncil/settings.py
+++ citycouncil/settings.py
@@ -54,8 +54,10 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     'fullurl',
     'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
