1344a90b847a7c3d7139565567262f72107563a5
Alex Riina <alex.riina@gmail.com>
2023-10-04 22:45:56 -0400

    add some crappy dockerfiles that dont have the right volumes set up

--- docker_settings.py
+++ docker_settings.py
@@ -17,6 +17,7 @@ DEBUG = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_compiled"),
 
     # outside of docker-compose volume
     "/static_compiled/",
