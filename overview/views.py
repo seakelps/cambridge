f6c18912c9cb46b6038c8411441c1acc2ab71f30
Alex Riina <alex.riina@gmail.com>
2023-10-05 22:38:51 -0400

    actually parse the description via markdown

--- overview/views.py
+++ overview/views.py
@@ -1,4 +1,5 @@
 import json
+from markdown import markdown
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView, TemplateView
@@ -96,7 +97,7 @@ class CandidateDetail(DetailView):
             "@type": "Article",
             "name": f"Learn more about {self.object.fullname}",
             "abstract": self.object.short_history_text,
-            "description": BeautifulSoup(self.object.blurb, features='html.parser').get_text(),
+            "description": BeautifulSoup(markdown(self.object.blurb), features='html.parser').get_text(),
             "image": self.object.headshot,
             "url": self.request.build_absolute_uri(self.object.get_absolute_url()),
             "thumbnailUrl": self.request.build_absolute_uri(self.object.headshot),
