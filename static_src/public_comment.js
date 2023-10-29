f5f0ffedfd06b134c12d3009896ae1bda4fad0e1
Alex Riina <alex.riina@gmail.com>
2023-10-28 22:32:27 -0400

    clean up public comment styling

--- static_src/public_comment.js
+++ static_src/public_comment.js
@@ -1,14 +1,36 @@
 function createTable() {
   var dt = new DataTable("#public-comment-table", {
     ordering: true,
-    paging: false,
+    paging: true,
     searching: true,
-    dom: "lfrtip",
+    dom: "frtip",
     pageLength: 15,
     columnDefs: [
-      { className: "dt-nowrap", targets: [ 0, 1 ] }
+      {
+        className: "dt-nowrap",
+        target: 0,
+        sortable: true,
+      },
+      {
+        className: "dt-nowrap",
+        target: 1,
+        render: comment_link,
+        sortable: true,
+      },
+      {
+        target: 2,
+        sortable: false,
+      },
+      {
+        target: 3,
+        visible: false,
+      },
     ]
   });
+}
+
+function comment_link( data, type, row ) {
+    return `<a target="_blank" rel="noopener" href="${row[3]}">${data}</a>`;
 }
 
 // export { createTable } uh couldn't get this working
