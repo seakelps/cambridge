2aceac56f66b362b33fbeb05df09a798e18ae7dd
Alex Riina <alex.riina@gmail.com>
2025-09-20 19:02:15 -0400

    more fixes

--- ranking/context_processors.py
+++ ranking/context_processors.py
@@ -8,47 +8,48 @@ from .models import RankedList, RankedElement
 
 
 def sidebar(request):
+    context = {}
     ranking_lookup = defaultdict(lambda: {"order": None, "comment": ""})
 
-    # TODO
-    council_election = Election.objects.filter(
-        year=settings.ELECTION_DATE.year,
-        position="council").first()
+    try:
+        election = Election.objects.get(
+            year=request.resolver_match.kwargs["year"],
+            position=request.resolver_match.kwargs["position"],
+        )
+        ranked_list = RankedList.objects.for_request(
+            request,
+            election=election,
+            force=False
+        )
+        annotated_candidates = (
+            ranked_list.annotated_candidates.all() if ranked_list else RankedElement.objects.none()
+        )
 
-    school_election = Election.objects.filter(
-        year=settings.ELECTION_DATE.year,
-        position="school"
-    ).first()
+        for ac in annotated_candidates.all():
+            ranking_lookup[ac.candidate] = {"comment": ac.comment, "order": ac.order}
 
-    ranked_list = RankedList.objects.for_request(request, election=council_election, force=False)
-    annotated_candidates = (
-        ranked_list.annotated_candidates.all() if ranked_list else RankedElement.objects.none()
-    )
+        context.update({
+            "my_ranking": ranked_list,
+            "my_candidates": [element.candidate for element in annotated_candidates],
+            "runners": CandidateElection.objects.filter(is_running=True, hide=False),
+            "runnerJson": json.dumps(
+                [
+                    {
+                        "slug": c.candidate.slug,
+                        "name": c.candidate.fullname,
+                        "blurb": markdown(c.blurb),
+                        "img_url": c.headshot.url if c.headshot else None,
+                        "img_alt": c.headshot_description,
+                        "comment": ranking_lookup[c]["comment"],
+                        "order": ranking_lookup[c]["order"],
+                    }
+                    for c in CandidateElection.objects.filter(is_running=True, hide=False).select_related("candidate")
+                ]
+            ),
+        })
 
-    for ac in annotated_candidates.all():
-        ranking_lookup[ac.candidate] = {"comment": ac.comment, "order": ac.order}
-
-    context = {
-        "my_ranking": ranked_list,
-        "my_candidates": [element.candidate for element in annotated_candidates],
-        "runners": CandidateElection.objects.filter(is_running=True, hide=False),
-        "runnerJson": json.dumps(
-            [
-                {
-                    "slug": c.candidate.slug,
-                    "name": c.candidate.fullname,
-                    "blurb": markdown(c.blurb),
-                    "img_url": c.headshot.url if c.headshot else None,
-                    "img_alt": c.headshot_description,
-                    "comment": ranking_lookup[c]["comment"],
-                    "order": ranking_lookup[c]["order"],
-                }
-                for c in CandidateElection.objects.filter(is_running=True, hide=False).select_related("candidate")
-            ]
-        ),
-    }
-
-    if request.GET.get("sidebar") == "true":
-        context["sidebar_visible"] = True
-
+        if request.GET.get("sidebar") == "true":
+            context["sidebar_visible"] = True
+    except (KeyError, Election.DoesNotExist):
+        pass
     return context
